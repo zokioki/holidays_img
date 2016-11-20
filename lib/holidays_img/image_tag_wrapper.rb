a1d0e35a84c265c3a31db514285657a33ea2c573
Zoran <zoran1991@gmail.com>
2016-11-19 23:19:04 -0800

    use original image file as fallback if holiday-specific file is not found

--- lib/holidays_img/image_tag_wrapper.rb
+++ lib/holidays_img/image_tag_wrapper.rb
@@ -5,10 +5,15 @@ module HolidaysImg
       holiday = Holidays.on(date, HolidaysImg.region).first
 
       if holiday
+        original_source = source.dup
         substring_index = source.rindex('.')
 
         if HolidaysImg.whitelist.empty? || HolidaysImg.whitelist.include?(holiday.name)
           source.insert(substring_index, "_#{holiday[:name].parameterize('_')}")
+
+          unless File.exists?("#{Rails.root}/app/assets/images/#{source}")
+            source = original_source
+          end
         end
       end
 
